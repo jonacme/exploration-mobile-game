e75b10543749e7161c5c56b8599659f615a80f03
yusal91 <94690560+yusal91@users.noreply.github.com>
2022-08-13 12:12:35 +0200

    no gridMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    Rigidbody2D rb2D;
+
+    public float horizontal;
+    public float vertical;
+    public float moveLimiter = 0.7f;
+
+    public float runSpeed = 20.0f;
+
     void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Gives a value between -1 and 1
+        horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
+        vertical = Input.GetAxisRaw("Vertical"); // -1 is down
+    }
+
+    void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
+        {
+            // limit movement speed diagonally, so you move at 70% speed
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+
+        rb2D.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
 }
